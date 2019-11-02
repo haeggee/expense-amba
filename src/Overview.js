413fabd1057e465df527119cb5e791771c5cd7c7
Brandon <brandon.dang1234@gmail.com>
2019-11-01 21:19:44 -0400

    fixed weird spacing issues that are leading to clutter

--- src/Overview.js
+++ src/Overview.js
@@ -13,64 +13,65 @@ import Group from './Group';
 
 
 const useStyles = makeStyles(theme => ({
-    gridcontainer: {
-        flexGrow: 1,
-        padding: theme.spacing(2),
-        minHeight: 750,
-        background: '#FFFFFF'
-    },
+  gridcontainer: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+    minHeight: 750,
+    background: '#FFFFFF'
+  },
 
-    // the style for the left grouplist paper
-    paperGroupList: {
-        color: theme.palette.text.secondary,
-        background: '#FFFFFF'
-    },
-    // the style for the expense diagram paper
-    paperGroupOverview: {
-        // color: theme.palette.text.secondary,
-        background: '#FFFFFF'
-    },
-    AppBar: {
-        position: 'relative'
-    },
-    title: {
-        marginLeft: theme.spacing(3),
-        marginTop: theme.spacing(1),
-        marginBottom: theme.spacing(1),
-        flex: 1,
-    },
-    subtitle: {
-        marginBottom: theme.spacing(1),
-        marginLeft: theme.spacing(3),
-        flex: 1
-    },
-    addButton: {
-        marginBottom: theme.spacing(2),
-        padding: theme.spacing(2)
-    }
+  // the style for the left grouplist paper
+  paperGroupList: {
+    color: theme.palette.text.secondary,
+    background: '#FFFFFF'
+  },
+  // the style for the expense diagram paper
+  paperGroupOverview: {
+    // color: theme.palette.text.secondary,
+    background: '#FFFFFF'
+  },
+  AppBar: {
+    position: 'relative'
+  },
+  title: {
+    marginLeft: theme.spacing(3),
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    flex: 1,
+  },
+  subtitle: {
+    marginBottom: theme.spacing(1),
+    marginLeft: theme.spacing(3),
+    flex: 1
+  },
+  addButton: {
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(2)
+  }
 }));
+
 /*  Returns a string of all the members of a group.
     @param {group} the group object
     @param {currentUser} The user that is using this app that is a part of this group
     We assume a group has at least one member.
 */
 const groupMembersString = function (group, currentUser) {
-    let text = "You"
-    
-    for (let i = 1; i < group.groupMembers.length; i++) {
-        // all usernames are unique, so we can use them to compare users
-        if (currentUser.username != group.groupMembers[i].username) {
-            // only add to string if this is not the current user
-            text += ", " + group.groupMembers[i].name;
-        }
+  let text = "You"
+  
+  for (let i = 1; i < group.groupMembers.length; i++) {
+    // all usernames are unique, so we can use them to compare users
+    if (currentUser.username != group.groupMembers[i].username) {
+      // only add to string if this is not the current user
+      text += ", " + group.groupMembers[i].name;
     }
-    return text;
+  }
+  return text;
 }
 
 function a11yProps(index) {
-    return {
-        id: `scrollable-auto-tab-${index}`, 'aria-controls': `scrollable-auto-tabpanel-${index}`,
-    };
+  return {
+    id: `scrollable-auto-tab-${index}`, 'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
 }
 
 
@@ -108,140 +109,137 @@ export function Overview(props) {
 
 	*/
 
-    // A possible way of implementing it?
-    const members = [new User("Alice", "password", "Alice", "Alice.gmail.com"), new User("Bob", "password", "Bob", "Bob.gmail.com"), new User("James", "password", "James", "James.gmail.com"),
-    new User("Maria", "password", "Maria", "Maria.gmail.com"), new User("Thomas", "password", "Thomas", "Thomas.gmail.com"), new User("Jennifer", "password", "Jennifer", "Jennifer.gmail.com")]
-    const billsGroup1 =
-        [new Bill(0, "Uber", 20.0, new Date('2019-10-01'), members[0], members),
-        new Bill(1, "Dinner", 35.0, new Date('2019-10-12'), members[1], [members[0], members[1], members[2]]),
-        new Bill(2, "Movie tickets", 15.0, new Date('2019-10-25'), members[4], [members[4], members[0], members[5]])]
-    const groups = [new Group(0, "Family", members, billsGroup1), new Group(1, "TO", [members[0], members[2], members[3], members[4], members[5]]), new Group(2, "Team 42", [members[0], members[1]])]
+  // A possible way of implementing it?
+  const members = [new User("Alice", "password", "Alice", "Alice.gmail.com"), new User("Bob", "password", "Bob", "Bob.gmail.com"), new User("James", "password", "James", "James.gmail.com"),
+  new User("Maria", "password", "Maria", "Maria.gmail.com"), new User("Thomas", "password", "Thomas", "Thomas.gmail.com"), new User("Jennifer", "password", "Jennifer", "Jennifer.gmail.com")]
+  const billsGroup1 =
+      [new Bill(0, "Uber", 20.0, new Date('2019-10-01'), members[0], members),
+      new Bill(1, "Dinner", 35.0, new Date('2019-10-12'), members[1], [members[0], members[1], members[2]]),
+      new Bill(2, "Movie tickets", 15.0, new Date('2019-10-25'), members[4], [members[4], members[0], members[5]])]
+  const groups = [new Group(0, "Family", members, billsGroup1), new Group(1, "TO", [members[0], members[2], members[3], members[4], members[5]]), new Group(2, "Team 42", [members[0], members[1]])]
 
-    // Let this be the current user.
-    const user = members[0]
+  // Let this be the current user.
+  const user = members[0]
 
-    /* END OF MOCK DATA ----------------------*/
+  /* END OF MOCK DATA ----------------------*/
 
-    // openPayments indicates whether or not to open the payments dialog popup
+  // openPayments indicates whether or not to open the payments dialog popup
 
-    const [openPayments, setopenPayments] = React.useState(false);
+  const [openPayments, setopenPayments] = React.useState(false);
 
-    /* 
+  /* 
 	Handles whenever payment button is clicked to open dialog to make payment.
 	*/
-    const openPaymentsDialog = () => {
-        setopenPayments(true)
-    };
+  const openPaymentsDialog = () => {
+    setopenPayments(true)
+  };
 
 	/* 
 	Handles closing dialog when Dialog requests it.
 	*/
-    const closePaymentsDialog = () => {
-        setopenPayments(false)
-    };
+  const closePaymentsDialog = () => {
+    setopenPayments(false)
+  };
 
-    /*
-    index of the current group
-    */
-    const [selectedIndex, setSelectedIndex] = React.useState(0);
+  /*
+  index of the current group
+  */
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-    /* Handles whenever another group is clicked */
+  /* Handles whenever another group is clicked */
 
-    const handeListItemClick = (event, index) => {
-        setSelectedIndex(index);
-    };
-
-
-
-    // which tab we are on
-    const [tabIndex, setTabIndex] = React.useState(0);
-
-    // what to do when user clicks on new tab
-    const handleTabChange = (event, newTabIndex) => {
-        setTabIndex(newTabIndex);
-    };
-
-    // the styles for the components    
-    const classes = useStyles();
-
-    return (
-        <div>
-            <CustomHeader />
-            <Container maxWidth="lg">
-                <Card className={classes.gridcontainer}>
-                    <Grid container spacing={2}>
+  const handeListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
 
 
-                        {/*left Grid that shows the grouplist*/}
-                        <Grid item xs={4}>
-                            <Paper className={classes.paperGroupList}>
-                                <AppBar className={classes.AppBar}>
-                                    <Typography variant="h6" className={classes.title}>
-                                        <strong>Your groups</strong>
-                                    </Typography>
-                                </AppBar>
-                                <GroupList
-                                    groups={groups}
-                                    groupID={selectedIndex}
-                                    handeListItemClick={handeListItemClick}
-                                />
-                                <Divider />
-                                <ListItem className={classes.addButton}>
-                                    <CustomButton>Create new group</CustomButton>
-                                </ListItem>
-                            </Paper>
-                        </Grid>
+
+  // which tab we are on
+  const [tabIndex, setTabIndex] = React.useState(0);
+
+  // what to do when user clicks on new tab
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+
+  // the styles for the components    
+  const classes = useStyles();
+
+  return (
+    <div>
+      <CustomHeader />
+      <Container maxWidth="lg">
+        <Card className={classes.gridcontainer}>
+          <Grid container spacing={2}>
+            {/*left Grid that shows the grouplist*/}
+            <Grid item xs={4}>
+              <Paper className={classes.paperGroupList}>
+                <AppBar className={classes.AppBar}>
+                  <Typography variant="h6" className={classes.title}>
+                    <strong>Your groups</strong>
+                  </Typography>
+                </AppBar>
+                <GroupList
+                  groups={groups}
+                  groupID={selectedIndex}
+                  handeListItemClick={handeListItemClick}
+                />
+                <Divider />
+                <ListItem className={classes.addButton}>
+                  <CustomButton>Create new group</CustomButton>
+                </ListItem>
+              </Paper>
+            </Grid>
 
 
-                        {/* right Grid that shows the overview and diagram */}
+            {/* right Grid that shows the overview and diagram */}
 
 
-                        <Grid item xs={8}>
-                            <Paper className={classes.paperGroupOverview}>
-                                <AppBar className={classes.AppBar}>
-                                    <Typography variant="h6" className={classes.title}>
-                                        <strong>{groups[selectedIndex].name}</strong>
-                                    </Typography>
-                                    <Typography variant="subtitle1" className={classes.subtitle}>
-                                        <em>Members:</em> {groupMembersString(groups[selectedIndex], user)}
-                                    </Typography>
+            <Grid item xs={8}>
+              <Paper className={classes.paperGroupOverview}>
+                <AppBar className={classes.AppBar}>
+                    <Typography variant="h6" className={classes.title}>
+                        <strong>{groups[selectedIndex].name}</strong>
+                    </Typography>
+                    <Typography variant="subtitle1" className={classes.subtitle}>
+                        <em>Members:</em> {groupMembersString(groups[selectedIndex], user)}
+                    </Typography>
+                </AppBar>
 
-                                </AppBar>
-                                <Box display="flex" flexDirection="row-reverse">
-                                    <Tabs value={tabIndex} onChange={handleTabChange}>
-                                        <Tab label="Balances" {...a11yProps(0)} />
-                                        <Tab label="Bills" {...a11yProps(1)} />
-                                    </Tabs>
-                                </Box>
+                <Box display="flex" flexDirection="row-reverse">
+                    <Tabs value={tabIndex} onChange={handleTabChange}>
+                        <Tab label="Balances" {...a11yProps(0)} />
+                        <Tab label="Bills" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
 
-                                <Balances
-                                    group={groups[selectedIndex]}
-                                    value={tabIndex}
-                                    index={0}
-                                />
+                <Balances
+                    group={groups[selectedIndex]}
+                    value={tabIndex}
+                    index={0}
+                />
 
-                                <BillList
-                                    group={groups[selectedIndex]}
-                                    value={tabIndex}
-                                    index={1}
-                                />
+                <BillList
+                    group={groups[selectedIndex]}
+                    value={tabIndex}
+                    index={1}
+                />
 
-                                <Container className={classes.addButton}>
-                                    <CustomButton clickHandler={openPaymentsDialog}>Add another payment</CustomButton>
-                                </Container>
-                                {/* Pass in handler that closes Dialog when the Dialog requests it. */}
-                                <PaymentDialog
-                                    open={openPayments}
-                                    closeHandler={closePaymentsDialog}
-                                    group={groups[selectedIndex]} />
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                </Card>
-            </Container>
-        </div>
-    );
-
-
+                <Container className={classes.addButton}>
+                    <CustomButton clickHandler={openPaymentsDialog}>Add another payment</CustomButton>
+                </Container>
+                {/* Pass in handler that closes Dialog when the Dialog requests it. */}
+                <PaymentDialog
+                    open={openPayments}
+                    closeHandler={closePaymentsDialog}
+                    group={groups[selectedIndex]}
+                    currentUser={user} />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Card>
+      </Container>
+    </div>
+  );
 }
 export default Overview;
