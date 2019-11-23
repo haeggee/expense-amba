e288632b882762a855cd3f2f42aa9a4b34aaac4c
haeggee <alexanderhaegele@t-online.de>
2019-11-23 16:02:27 -0500

    added server.js and basic server functionality

---
+++ server.js
@@ -0,0 +1,181 @@
+'use strict';
+const log = console.log
+
+const express = require('express')
+// starting the express server
+const app = express();
+
+// mongoose and mongo connection
+const { mongoose } = require('./db/mongoose')
+
+// import the mongoose models
+const { Bill } = require('./models/Bill')
+const { User } = require('./models/User')
+const { Group } = require('./models/Group')
+
+// to validate object IDs
+const { ObjectID } = require('mongodb')
+
+// body-parser: middleware for parsing HTTP JSON body into a usable object
+const bodyParser = require('body-parser') 
+app.use(bodyParser.json())
+
+// express-session for managing user sessions
+const session = require('express-session')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/build'))
+
+/*** Session handling **************************************/
+// Create a session cookie
+app.use(session({
+    secret: 'oursecret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60000,
+        httpOnly: true
+    }
+}));
+
+// Our own express middleware to check for 
+// an active user on the session cookie (indicating a logged in user.)
+const sessionChecker = (req, res, next) => {
+    if (req.session.user) {
+        res.redirect('/overview'); // redirect to dashboard if logged in.
+    } else {
+        next(); // next() moves on to the route.
+    }    
+};
+
+// A route to login and create a session
+app.post('/users/login', (req, res) => {
+	const email = req.body.email
+    const password = req.body.password
+
+    // Use the static method on the User model to find a user
+    // by their email and password
+	User.findByEmailPassword(email, password).then((user) => {
+	    if (!user) {
+            res.redirect('/');
+        } else {
+            // Add the user's id to the session cookie.
+            // We can check later if this exists to ensure we are logged in.
+            req.session.user = user._id;
+            res.redirect('/overview');
+        }
+    }).catch((error) => {
+		res.status(400).redirect('/');
+    })
+})
+
+// A route to logout a user
+app.get('/users/logout', (req, res) => {
+	// Remove the session
+	req.session.destroy((error) => {
+		if (error) {
+			res.status(500).send(error)
+		} else {
+			res.redirect('/')
+		}
+	})
+})
+
+
+/*** Webpage routes below **********************************/
+// Inject the sessionChecker middleware to any routes that require it.
+// sessionChecker will run before the route handler and check if we are
+// logged in, ensuring that we go to the dashboard if that is the case.
+
+// The various redirects will ensure a proper flow between login and dashboard
+// pages so that your users have a proper experience on the front-end.
+
+//***             open question: redirects do not work together with react-router-dom;
+//***             will probably have to use redirect features of react-router-dom*/
+app.get('/', sessionChecker, (req, res) => {
+	res.sendFile(__dirname + '/build/index.html')
+})
+
+// // login route serves the login page
+// app.get('/login', sessionChecker, (req, res) => {
+// 	res.redirect('/login')
+// })
+
+// // dashboard route will check if the user is logged in and server
+// // the dashboard page
+// app.get('/overview', (req, res) => {
+// 	if (req.session.user) {
+// 		res.redirect('/overview')
+// 	} else {
+// 		res.redirect('/login')
+// 	}
+
+// })
+
+// static js directory
+app.use("/js", express.static(__dirname + '/public/js'))
+
+/*********************************************************/
+
+/*** API Routes below ************************************/
+
+// a POST route to *create* a group
+app.post('/group', (req, res) => {
+	// log(req.body)
+	// TODO
+})
+
+// a GET route to get all groups
+app.get('/groups', (req, res) => {
+	// TODO
+})
+
+/// a GET route to get a group by their id.
+// id is treated as a wildcard parameter, which is why there is a colon : beside it.
+// (in this case, the database id, but you can make your own id system for your project)
+app.get('/group/:id', (req, res) => {
+	/// req.params has the wildcard parameters in the url, in this case, id.
+	// log(req.params.id)
+
+})
+
+/// a DELETE route to remove a group by their id.
+app.delete('/group/:id', (req, res) => {
+	const id = req.params.id
+	// TODO
+})
+
+app.delete('/bill/:id', (req, res) => {
+	const id = req.params.id
+	// TODO
+})
+
+// a PATCH route for changing properties of a resource.
+// (alternatively, a PUT is used more often for replacing entire resources).
+app.patch('/users/:id', (req, res) => {
+	const id = req.params.id
+	// TODO
+	
+})
+
+
+/** User routes below **/
+// Set up a POST route to *create* a user of the web app 
+app.post('/users', (req, res) => {
+	log(req.body)
+	// TODO
+})
+
+/*************************************************/
+// Express server listening...
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+}) 
+
+
+
+
+
+
+
